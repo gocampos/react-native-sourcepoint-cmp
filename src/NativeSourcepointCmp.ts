f8d436758f2bc6127c688d6b3c9f312f895a936c
André Herculano <andresilveirah@gmail.com>
2024-05-22 15:23:54 +0200

    feat: refactor SDK ts types to its own file

--- src/NativeSourcepointCmp.ts
+++ src/NativeSourcepointCmp.ts
@@ -1,42 +1,5 @@
-import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
-export type SPCampaign = {
-  targetingParams?: Object;
-  supportLegacyUSPString?: boolean;
-};
-
-export const enum SPCampaignEnvironment {
-  Public = 'Public',
-  Stage = 'Stage',
-}
-
-export type SPCampaigns = {
-  gdpr?: SPCampaign;
-  usnat?: SPCampaign;
-  environment?: SPCampaignEnvironment;
-};
-
-export interface Spec extends TurboModule {
-  build(
-    accountId: number,
-    propertyId: number,
-    propertyName: string,
-    campaigns: SPCampaigns
-  ): void;
-  getUserData(): Promise<Record<string, unknown>>;
-  loadMessage(): void;
-  clearLocalData(): void;
-  loadGDPRPrivacyManager(pmId: string): void;
-  loadUSNatPrivacyManager(pmId: string): void;
-
-  onFinished(callback: () => void): void;
-  onAction(callback: (action: string) => void): void;
-  onSPUIReady(callback: () => void): void;
-  onSPUIFinished(callback: () => void): void;
-  onError(callback: (description: string) => void): void;
-
-  dispose(): void;
-}
+import type { Spec } from './types';
 
 export default TurboModuleRegistry.getEnforcing<Spec>('RNSourcepointCmp');
---
+++ src/types.ts
@@ -0,0 +1,111 @@
+import type { TurboModule } from 'react-native';
+
+export type SPCampaign = {
+  targetingParams?: Object;
+  supportLegacyUSPString?: boolean;
+};
+
+export const enum SPCampaignEnvironment {
+  Public = 'Public',
+  Stage = 'Stage',
+}
+
+export type SPCampaigns = {
+  gdpr?: SPCampaign;
+  usnat?: SPCampaign;
+  environment?: SPCampaignEnvironment;
+};
+
+export interface CampaignConsent<Consent extends ConcreteConsent> {
+  applies: boolean;
+  consents: Consent;
+}
+
+export interface ConcreteConsent {
+  uuid?: String;
+  // TODO: uncomment once both GDPR and USNAT expose those two attributes
+  // expirationDate?: String;
+  // createdDate?: String;
+}
+
+export type GDPRConsentStatus = {
+  consentedAll?: Boolean;
+  consentedAny?: Boolean;
+  rejectedAny?: Boolean;
+};
+
+export type USNatConsentStatus = {
+  consentedAll?: Boolean;
+  consentedAny?: Boolean;
+  rejectedAny?: Boolean;
+  sellStatus?: Boolean;
+  shareStatus?: Boolean;
+  sensitiveDataStatus?: Boolean;
+  gpcStatus?: Boolean;
+};
+
+export type GDPRVendorGrant = {
+  granted: Boolean;
+  purposes: Record<string, Boolean>;
+};
+
+export type GDPRConsent = {
+  uuid?: String;
+  // expirationDate?: String;
+  // createdDate?: String;
+  euconsent?: String;
+  vendorGrants: Record<string, GDPRVendorGrant>;
+  statuses?: GDPRConsentStatus;
+  tcfData?: Object;
+};
+
+export type Consentable = {
+  consented: Boolean;
+  GGGGGGid: String;
+};
+
+export type ConsentSection = {
+  id: String;
+  name: String;
+  consentString: String;
+};
+
+export type USNatConsent = {
+  uuid?: String;
+  // expirationDate?: String;
+  // createdDate?: String;
+  consentSections: Array<ConsentSection>;
+  statuses?: USNatConsentStatus;
+  gppData?: Object;
+  vendors: [Consentable];
+  categories: [Consentable];
+};
+
+export type SPUserData = {
+  gdpr?: CampaignConsent<GDPRConsent>;
+  usnat?: CampaignConsent<USNatConsent>;
+};
+
+export interface Spec extends TurboModule {
+  build(
+    accountId: number,
+    propertyId: number,
+    propertyName: string,
+    campaigns: SPCampaigns
+  ): void;
+  getUserData(): Promise<SPUserData>;
+  loadMessage(): void;
+  clearLocalData(): void;
+  loadGDPRPrivacyManager(pmId: string): void;
+  loadUSNatPrivacyManager(pmId: string): void;
+
+  onFinished(callback: (userData: SPUserData) => void): void;
+
+  // TODO: change action from string to enum
+  onAction(callback: (action: string) => void): void;
+  onSPUIReady(callback: () => void): void;
+  onSPUIFinished(callback: () => void): void;
+  onError(callback: (description: string) => void): void;
+
+  dispose(): void;
+}
