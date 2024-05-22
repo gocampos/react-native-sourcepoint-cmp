96228713e28689cca7d3ca3da467ce6bd7a9a402
André Herculano <andresilveirah@gmail.com>
2024-05-22 15:24:53 +0200

    feat: refactor example app to use SPUserData type

--- example/src/UserDataView.tsx
+++ example/src/UserDataView.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
 
 import { TestableText } from './TestableText';
+import type { SPUserData } from '@sourcepoint/react-native-cmp';
 
 export default ({ data }: UserDataViewProps) => (
   <View style={styles.container}>
     <Text style={styles.header}>Local User Data</Text>
     <TestableText testID="gdpr.uuid">{data?.gdpr?.consents?.uuid}</TestableText>
     <TestableText testID="gdpr.consentStatus">
-      {data?.gdpr?.consents?.consentStatus?.consentedAll
+      {data?.gdpr?.consents?.statuses?.consentedAll
         ? 'consentedAll'
         : 'rejectedAll'}
     </TestableText>
@@ -16,37 +17,20 @@ export default ({ data }: UserDataViewProps) => (
       {data?.usnat?.consents?.uuid}
     </TestableText>
     <TestableText testID="usnat.consentStatus">
-      {/* TODO: cleanup below once consent classes are standardised */}
-      {data?.usnat?.consents?.consentStatus?.consentedToAll ||
-      data?.usnat?.consents?.consentStatus?.consentedAll
+      {data?.usnat?.consents?.statuses?.consentedAll
         ? 'consentedAll'
         : 'rejectedAll'}
     </TestableText>
     <ScrollView style={styles.container}>
       <Text testID="userData" style={styles.userDataText}>
-        {JSON.stringify(
-          {
-            gdpr: {
-              uuid: data?.gdpr?.consents?.uuid,
-              consentStatus: data?.gdpr?.consents?.consentStatus,
-              applies: data?.gdpr?.applies,
-            },
-            usnat: {
-              uuid: data?.usnat?.consents?.uuid,
-              consentStatus: data?.usnat?.consents?.consentStatus,
-              applies: data?.usnat?.applies,
-            },
-          },
-          null,
-          2
-        )}
+        {JSON.stringify(data, null, 2)}
       </Text>
     </ScrollView>
   </View>
 );
 
 type UserDataViewProps = {
-  data: Record<string | number | symbol, any>;
+  data: SPUserData;
 };
 
 const styles = StyleSheet.create({
